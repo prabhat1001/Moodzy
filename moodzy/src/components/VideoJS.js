1105e4353681dd3e34f292f5f6c4b2f33847840c
prabhat1001 <prabhatsaraswat24@gmail.com>
2022-05-29 23:47:35 +0530

    trailers enable

--- moodzy/src/components/VideoJS.js
+++ moodzy/src/components/VideoJS.js
@@ -1,23 +1,50 @@
 import styled from 'styled-components';
+import ReactPlayer from 'react-player';
+
 
 const VideoJS = (props) => {
+    const Url = localStorage.getItem('url');
+    console.log(Url);
+
     return (
         <Container>
-        <h4>VideoJS</h4>
+        <Player>
+            <ReactPlayer 
+                width={1370} 
+                height={580} 
+                playing={true}
+                controls url={Url}
+
+                //callbacks
+                onReady={() => console.log('onReady callback')}
+                onStart={() => console.log('onStart callback')}
+                onPause={() => console.log('onPause callback')}
+                onEnded={() => console.log('onEnded callback')}
+                onError={() => console.log('onError callback')}  />
+
+        </Player>
         </Container>
     );
 }
 
 const Container = styled.div`
-position: relative;
-text-align: left;
-min-height: calc(100vh - 70px);
-overflow-x: hidden;
-display: block;
-top: 70px;
-padding:  0 calc(3.5vw + 5px);
+    position: relative;
+    min-height: calc(100vh - 70px);
+    overflow-x: hidden;
+    overflow-y: hidden;
+    display: block;
+    top: 70px;
+    /* height: 79.3; */
 `;
 
+const Player = styled.div`
+/* position: absolute; */
+    display: flex;
+    align-items: center;
+    margin-left: auto;
+    margin-right: auto;
+
+`;
 
 
 export default VideoJS;
