d70eda196d5f398bdbed35f734fd0c771252a561
prabhat1001 <prabhatsaraswat24@gmail.com>
2022-05-29 15:09:03 +0530

    header updated

--- moodzy/src/components/Header.js
+++ moodzy/src/components/Header.js
@@ -71,27 +71,27 @@ const Header = (props)=>{
                     <span> HOME</span>
                 </a>
 
-                <a>
+                <a  href="/home">
                     <img src="/images/search-icon.svg" alt="SEARCH" />
                     <span> SEARCH</span>
                 </a>
 
-                <a>
+                < a href="/home">
                     <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
                     <span> WATCHLIST</span>
                 </a>
 
-                <a>
+                <a  href="/home">
                     <img src="/images/original-icon.svg" alt="ORIGINALS" />
                     <span> ORIGINALS</span>
                 </a>
 
-                <a>
+                <a href="/home">
                     <img src="/images/movie-icon.svg" alt="MOVIES" />
                     <span> MOVIES</span>
                 </a>
 
-                <a>
+                <a href="/home">
                     <img src="/images/series-icon.svg" alt="SERIES" />
                     <span> SERIES</span>
                 </a>
