a15c2c66cca024246672b4b79dc8b59e740bc286
prabhat1001 <prabhatsaraswat24@gmail.com>
2022-05-29 15:01:13 +0530

    App.js updated

--- moodzy/src/App.js
+++ moodzy/src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header"
 import './App.css';
 import Home from "./components/Home";
 import Details from "./components/Details";
+import Collab from "./components/Collab";
+import VideoJS from "./components/VideoJS";
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
         <Routes> {/*In react-router-dom v6, "Switch" is replaced by routes "Routes". */}
           <Route exact path="/" element={<Login />}></Route>
           <Route path ='/home' element={<Home/>}></Route> 
-          <Route path ='/details/:id' element={<Details/>}></Route>
+          <Route path ='/detail/:id' element={<Details/>}></Route>
+          <Route path ='/collab/' element={<Collab/>}></Route>
+          <Route path ='/player/' element={<VideoJS/>}></Route>
         </Routes>
       </Router>
     </div>
