8084752443f4cd2023f0a78a71f62d0fea7bd145
prabhat1001 <prabhatsaraswat24@gmail.com>
2022-05-30 01:51:22 +0530

    updated

--- moodzy/src/App.js
+++ moodzy/src/App.js
@@ -6,6 +6,11 @@ import Home from "./components/Home";
 import Details from "./components/Details";
 import Collab from "./components/Collab";
 import VideoJS from "./components/VideoJS";
+import Marvel from "./components/Marvel";
+import Disney from "./components/Disney";
+import Pixar from "./components/Pixar";
+import NatGeo from "./components/NatGeo";
+import StarWar from "./components/StarWar";
 
 function App() {
   return (
@@ -15,6 +20,11 @@ function App() {
         <Routes> {/*In react-router-dom v6, "Switch" is replaced by routes "Routes". */}
           <Route exact path="/" element={<Login />}></Route>
           <Route path ='/home' element={<Home/>}></Route> 
+          <Route path ='/disney' element={<Disney/>}></Route> 
+          <Route path ='/pixar' element={<Pixar/>}></Route> 
+          <Route path ='/marvel' element={<Marvel/>}></Route> 
+          <Route path ='/star-war' element={<StarWar/>}></Route> 
+          <Route path ='/national-geographic' element={<NatGeo/>}></Route> 
           <Route path ='/detail/:id' element={<Details/>}></Route>
           <Route path ='/collab/' element={<Collab/>}></Route>
           <Route path ='/player' element={<VideoJS/>}></Route>
